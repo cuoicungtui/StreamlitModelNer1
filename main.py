120bd53a361e0c007be842e1ad410885c58f75c5
cuoicungtui <tuongdangvuongquoc901@gmail.com>
2023-06-14 11:53:06 +0700

    fix mongo

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ text = st.text_input('Nhập câu nói ', '', max_chars=3000)
 
 # path_atlas = 'mongodb+srv://cuoicungtui:6V0lb3R2MnFKH6op@cluster0.kte4zsw.mongodb.net/'
 # path_atlas = os.environ.get('SRV_MONGO')
-path_atlas = env_variables['SRV_MONGO']
+path_atlas = env_variables['SRV_PY_MONGO']
 
 datamongo = dataMongo(path_atlas)
 if len(text) > 0:
